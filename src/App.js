59c3a55b58fc173665c5a3ba4e89ad527b37b44c
medahad <medahad16@gmail.com>
2023-04-07 21:23:03 +0200

    Initialization of PE-APP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [adBlockEnabled, setAdBlockEnabled] = React.useState(false);
+  const [cookieManagementEnabled, setCookieManagementEnabled] = React.useState(false);
+  const [ipMaskingEnabled, setIpMaskingEnabled] = React.useState(false);
+  const [antiTrackingEnabled, setAntiTrackingEnabled] = React.useState(false);
+  const [customizationEnabled, setCustomizationEnabled] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="privacy-extension">
+      <h2>Privacy Extension</h2>
+      <div className="options">
+        <label>
+          <input type="checkbox" checked={adBlockEnabled} onChange={() => setAdBlockEnabled(!adBlockEnabled)} />
+          Ad-blocking
+        </label>
+        <label>
+          <input type="checkbox" checked={cookieManagementEnabled} onChange={() => setCookieManagementEnabled(!cookieManagementEnabled)} />
+          Cookie management
+        </label>
+        <label>
+          <input type="checkbox" checked={ipMaskingEnabled} onChange={() => setIpMaskingEnabled(!ipMaskingEnabled)} />
+          IP masking
+        </label>
+        <label>
+          <input type="checkbox" checked={antiTrackingEnabled} onChange={() => setAntiTrackingEnabled(!antiTrackingEnabled)} />
+          Anti-tracking
+        </label>
+        <label>
+          <input type="checkbox" checked={customizationEnabled} onChange={() => setCustomizationEnabled(!customizationEnabled)} />
+          Customization options
+        </label>
+      </div>
     </div>
   );
 }
